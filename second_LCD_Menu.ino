f78f3a9a7876fec3451597d95d899cabafada50a
Hasnain Haider <hussu2001@gmail.com>
2016-05-11 19:50:55 -0700

    New!

--- second_LCD_Menu.ino
+++ second_LCD_Menu.ino
@@ -14,19 +14,24 @@
 #define RS        8
 #define ENABLE    9
 #define BACKLIGHT 10
+
+/************************/
 #define NUMBEROPTIONS 4
+#define TOGGLEDISPLAYOPTION 3
+
   /* 
-Analog 0	Button (select, up, right, down and left)
-Digital 4	DB4
-Digital 5	DB5
-Digital 6	DB6
-Digital 7	DB7
-Digital 8	RS (Data or Signal Display Selection)
-Digital 9	Enable
-Digital 10	Backlit Control 
+Analog 0  Button (select, up, right, down and left)
+Digital 4 DB4
+Digital 5 DB5
+Digital 6 DB6
+Digital 7 DB7
+Digital 8 RS (Data or Signal Display Selection)
+Digital 9 Enable
+Digital 10  Backlit Control 
 */
 
 boolean changed            = false;
+//triangle
 byte customChar[8] = {
     0b01000,
     0b01100,
@@ -38,20 +43,15 @@ byte customChar[8] = {
     0b00000
 };
 uint8_t btnPressed          = btnNONE;
-uint8_t currentMenuItem  = 0;
-String menu[NUMBEROPTIONS] = { "Option 1", "Option 2", "Option 3", "Option 4"};
+int currentMenuItem  = 0;
+String menu[NUMBEROPTIONS] = { "Option 1", "Option 2", "Option 3", "Toggle Display"};
 int btnRange[]           = { 60,200,400,600,800 };
 LiquidCrystal lcd(RS, ENABLE, DB4, DB5, DB6, DB7);
-
+boolean displayon;
 uint8_t readLCDBtn()
 {
     int x = analogRead(0);
     uint8_t choice = btnNONE;
-    if (DEBUGMODE)
-    {
-        lcd.setCursor(1, 0);
-        lcd.print(x);
-    }
     if (x < btnRange[btnRIGHT])
         choice = btnRIGHT;
     else if (x < btnRange[btnUP])
@@ -71,6 +71,7 @@ void setup()
     lcd.createChar((uint8_t)0, customChar);
     lcd.noCursor();
     displayMenu();
+    displayon = true;
 }
 
 void loop()
@@ -78,7 +79,7 @@ void loop()
     btnPressed = readLCDBtn();
     if (btnPressed != btnNONE)
         processChoice(btnPressed);
-    delay(50);
+    delay(200);
 }
 
 void displayMenu()
@@ -86,10 +87,10 @@ void displayMenu()
     lcd.clear();
     lcd.setCursor(0, 0);
     lcd.write((uint8_t)0);
-    lcd.setCursor(0, 1);
+    lcd.setCursor(1, 0);
     lcd.print(menu[currentMenuItem]);
-    lcd.setCursor(1,0);
-    lcd.print(menu[(currentMenuItem+1) % NUMBEROPTIONS]);
+    lcd.setCursor(0,1);
+    lcd.print(menu[(currentMenuItem + 1) % NUMBEROPTIONS]);
 }
 
 
@@ -115,10 +116,25 @@ void processChoice(uint8_t selection)
     case btnLEFT:
         break;
     case btnSELECT:
+        doOption();
+        break;
     default:
         return;
         break;
     }
-    Serial.print(btnPressed);
     btnPressed = btnNONE;
+}
+
+void doOption()
+{
+  switch(currentMenuItem)
+  {
+    case 3:
+        (displayon)? (lcd.noDisplay()) : (lcd.display());
+        displayon = !displayon;
+        break;
+    default:
+        break;
+  }
+
 }
